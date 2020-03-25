58f5015527b062b3b3a27e66487cd953e0fe7295
Andrei Sadovskii <and.sadovsckij@yandex.ru>
2020-03-25 14:07:06 +0400

    Init commit

---
+++ src/main/java/inc/sad/dataJoiner/Config.scala
@@ -0,0 +1,14 @@
+package inc.sad.dataJoiner
+
+case class Config(applicationConfig: ApplicationConfig, kafkaConfig: KafkaConfig)
+
+case class ApplicationConfig(consumerPoolSize: Int)
+
+case class KafkaConfig(sourceTopic: String,
+                       targetTopic: String,
+                       applicationId: String,
+                       commitInterval: String,
+                       autoOffsetReset: String,
+                       maxPoolRecords: String,
+                       bootstrapServers: List[String])
+
